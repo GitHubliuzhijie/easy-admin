12ad62367c1028081b4a331b7937a54ae3e3e47d
liuzhijie <liuzhijie.email@qq.com>
2025-07-28 10:19:16 +0800

    feat: 初始化 easy-admin 项目结构
    - 新增 .env 文件，配置项目基本信息
    - 修改 App.vue，删除默认模板
    - 移除 HelloWorld.vue 组件
    - 更新 index.html，添加项目元信息和 noscript 提示
    - 新增 src/style/index.scss，引入 normalize.css 和 element-plus 样式
    - 更新 main.js，引入新的样式文件
    - 修改 package.json，添加 element-plus、sass、unplugin-auto-import 等依赖
    - 重写 vite.config.js，配置 ElementPlus 解析器和路径别名

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        })
+    ],
+    css: {
+        preprocessorOptions: {
+            scss: {api: 'modern-compiler'},
+        }
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'), // 配置 @ 指向 src 目录
+        },
+    },
 })
