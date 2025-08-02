999fa28a7a0d475257ae3e5163cc3ea5cacc78ee
liuzhijie <liuzhijie.email@qq.com>
2025-08-03 07:46:32 +0800

    feat(vite.config): 动态配置服务端口
    - 引入 loadEnv 函数以支持环境变量加载
    - 使用环境变量VITE_SERVER_PORT 配置服务端口，默认为 3000
    - 优化了配置文件结构，采用 ({mode}) => {} 的方式定义配置

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -8,48 +8,52 @@ import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
 
-export default defineConfig({
-    server: {
-        host: '0.0.0.0', // 允许外部访问
-        port: 3000,      // 指定端口
-    },
-    plugins: [
-        vue(),
-        UnoCSS(),
-        AutoImport({
-            imports: [
-                'vue', // 自动导入 Vue 的 API，如 ref、reactive 等
-            ],
-            resolvers: [
-                ElementPlusResolver(),
-                IconsResolver()
-            ],
-        }),
-        Components({
-            resolvers: [
-                ElementPlusResolver({importStyle: 'sass'}),
-                IconsResolver({
-                    prefix: 'i', // 默认前缀是‘i’
-                    enabledCollections: ['ep'] // 使用哪个图标库
-                })
-            ],
-        }),
-        Icons({
-            compiler: 'vue3',// 指定编译器
-            // 自动安装图标库
-            autoInstall: true
-        })
-    ],
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `@use "@/styles/element-variables.scss" as *;`,
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    const serverPort = env.VITE_SERVER_PORT || 3000;
+    return {
+        server: {
+            host: '0.0.0.0', // 允许外部访问
+            port: serverPort,      // 指定端口
+        },
+        plugins: [
+            vue(),
+            UnoCSS(),
+            AutoImport({
+                imports: [
+                    'vue', // 自动导入 Vue 的 API，如 ref、reactive 等
+                ],
+                resolvers: [
+                    ElementPlusResolver(),
+                    IconsResolver()
+                ],
+            }),
+            Components({
+                resolvers: [
+                    ElementPlusResolver({importStyle: 'sass'}),
+                    IconsResolver({
+                        prefix: 'i', // 默认前缀是‘i’
+                        enabledCollections: ['ep'] // 使用哪个图标库
+                    })
+                ],
+            }),
+            Icons({
+                compiler: 'vue3',// 指定编译器
+                // 自动安装图标库
+                autoInstall: true
+            })
+        ],
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `@use "@/styles/element-variables.scss" as *;`,
+                },
             },
         },
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'), // 配置 @ 指向 src 目录
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'), // 配置 @ 指向 src 目录
+            },
         },
-    },
+    }
 })
