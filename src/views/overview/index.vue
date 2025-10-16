75ad763e8acdaf52b55d27b8fe81553556e0df57
liuzhijie <liuzhijie.email@qq.com>
2025-10-16 09:59:35 +0800

    style(overview): 调整页面容器宽度以优化布局
    -为页面容器添加固定宽度类名 `w-100` 以改善内容对齐
    - 维持原有居中布局结构与样式一致性

--- src/views/overview/index.vue
+++ src/views/overview/index.vue
@@ -9,7 +9,7 @@ defineOptions({
 <template>
   <div class="p-4 h-full">
     <div class="h-full rounded-lg bg-[var(--easy-color-bg-container)] flex justify-center items-center">
-      <div class="text-center">
+      <div class="text-center w-100">
         <h1 class="text-center mb-6">开启牛马的一天</h1>
         <div class="flex justify-center">
           <working-svg class="h-full text-[var(--easy-color-text)]"></working-svg>
